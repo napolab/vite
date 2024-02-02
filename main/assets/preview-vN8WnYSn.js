9b85ac09e51759597f4fdd81f70c9ad4814e2136
naporin0624 <naporin0624@gmail.com>
2024-02-02 11:29:35 +0000

    Deploying to gh-pages from @ napolab/vite@6fe302a068cc3a78b4c53a551777b9420b94f501 🚀

---
+++ main/assets/preview-vN8WnYSn.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-6IyBsWOF.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-aWjtcNu3.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]));return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-aWjtcNu3.js","assets/iframe-6IyBsWOF.js","assets/index-XiNr8FW2.js","assets/_commonjsHelpers-5-cIlDoe.js","assets/react-18-sFOusbzT.js","assets/index-9vG4XYWr.js","assets/index-ogXoivrg.js","assets/extends-dGVwEr9R.js","assets/_getPrototype-4FkL748a.js","assets/index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
